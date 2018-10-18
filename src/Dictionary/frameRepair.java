7143da50d80483c0351aeaf078a7302f6c489b18
trunganhvu <37219817+trunganhvu@users.noreply.github.com>
2018-10-18 13:32:17 +0700

    Update frameRepair.java

--- src/Dictionary/frameRepair.java
+++ src/Dictionary/frameRepair.java
@@ -2,49 +2,64 @@
 package dictionary;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import javax.swing.*;
 
 public class frameRepair extends DictionaryManagement{
     @SuppressWarnings("empty-statement")
-    public void repair(){
-        JFrame delFrame = new JFrame();
-        JButton okButton;
-        JButton cancelButton;
-        JLabel Eng,Vie,tWord;
-        JTextField engText, vieText;
+    private final JFrame reFrame = new JFrame();
+    private JButton okButton;
+    private JButton cancelButton;
+    private JLabel Eng,Vie;
+    private JLabel wordDic;
+    public JTextField engText, vieText;
+    private JTextField wordOld;
+    @SuppressWarnings("empty-statement")
+    public void repair(){   
         // init
         engText = new JTextField();
         vieText = new JTextField();
+        wordOld = new JTextField();
         okButton = new JButton("OK");
         cancelButton = new JButton("Cancel");;
         Eng = new JLabel("English");
         Vie = new JLabel("Vietnam");
+        wordDic = new JLabel("Word");
         okButton.setBounds(115,220,80,30);
         cancelButton.setBounds(210,220,80,30);
-        Eng.setBounds(10, 5, 50, 50);
-        engText.setBounds(75, 15, 200, 30);
-        Vie.setBounds(10, 40, 70, 50);
+
         vieText.setBounds(75, 50, 200, 30);
+        engText.setBounds(75, 15, 200, 30);
+        wordOld.setBounds(75, 90, 200, 30);
+        Eng.setBounds(10, 5, 50, 50);
+        Vie.setBounds(10, 40, 70, 50);
+        wordDic.setBounds(10, 75, 50, 50);
         
-        delFrame.add(Eng);
-        delFrame.add(Vie);
-        delFrame.add(engText);
-        delFrame.add(vieText);
-        delFrame.add(okButton);
-        delFrame.add(cancelButton);
-        delFrame.setTitle("Repair Your Word");
-        delFrame.setSize(300, 300);
-        delFrame.setLocation(500, 200);
-        delFrame.setResizable(false);  
-        delFrame.setLayout(null);
-        delFrame.setVisible(true);  
+        reFrame.add(wordDic);
+        reFrame.add(wordOld);
+        reFrame.add(Eng);
+        reFrame.add(Vie);
+        reFrame.add(engText);
+        reFrame.add(vieText);
+        reFrame.add(okButton);
+        reFrame.add(cancelButton);
+        reFrame.setTitle("Repair Your Word");
+        reFrame.setSize(300, 300);
+        reFrame.setLocation(500, 200);
+        reFrame.setResizable(false);  
+        reFrame.setLayout(null);
+        reFrame.setVisible(true);  
         // action
-        cancelButton.addActionListener((ActionEvent e) -> {
-            delFrame.setVisible(false);
+        cancelButton.addActionListener((ActionEvent e) -> {//nut thoat
+            reFrame.setVisible(false);
         });
-        okButton.addActionListener((ActionEvent e) -> {
-            
-            delFrame.setVisible(false);
+        okButton.addActionListener((ActionEvent e) -> { //nut ok de sua tu
+            super.repairWordFrame(wordOld.getText(),engText.getText(), vieText.getText());
+            reFrame.setVisible(false);
+            try {
+                dictionaryExportToFile();
+            } catch (IOException ex) {
+            }
         });
     }
 }
